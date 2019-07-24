77ac0f13947796234723fe624130ef441f690556
dbueno96 <daniel.bueno@correounivalle.edu.co>
2019-07-24 14:48:57 -0500

    Bash v-1.0.0

--- reductor.sh
+++ reductor.sh
@@ -1,4 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
+cd Reductor
 
-/bin/bash $(pwd)/Reductor/run.sh
+sudo apt-get update
+sudo apt-get install openjdk-8-jdk
+sudo apt-get install maven
+mvn clean install
+mvn spring-boot:run
